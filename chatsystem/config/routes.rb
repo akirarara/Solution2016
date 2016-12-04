2fe854d1368aa9de1bfcdfcf3e6807ccdb99b5d1
Apple <apple@AppledeMacBook-Pro.local>
2016-12-05 03:25:18 +0900

    チャットルーム機能の実装。

--- chatsystem/config/routes.rb
+++ chatsystem/config/routes.rb
@@ -1,10 +1,15 @@
 Chatsystem::Application.routes.draw do
+  get 'rooms/show'
+
     resources :users, only: [:index, :show, :new, :edit, :create, :update]
     resources :sessions, only: [:new, :create, :destroy]
     
     match 'signup', to: 'users#new', via: 'get'
     match 'signin', to: 'sessions#new', via: 'get'
     match 'signout', to: 'sessions#destroy', via: 'delete'
+    match 'chatroom', to: 'rooms#show', via: 'get'
     
     root 'users#index'
+    
+    mount ActionCable.server => '/cable' # Action Cable の有効化
 end
